6a560ec24cf0042443590ca4f9141204624fe7af
Vlad-Pelmeshka <vcheredaiko@gmail.com>
2025-05-09 17:52:53 +0300

    setup 1

--- 404.php
+++ 404.php
@@ -6,17 +6,22 @@
             <div class="not-found__content">
                 <div class="not-found__content-inner">
                     <?php if ($lg = get_field('logo_404', 'option')): ?>
-                    <div class="not-found__logo">
-                        <img src="<?php echo $lg['url']; ?>" alt="Logo 404">
-                    </div>
-
+                        <div class="not-found__logo">
+                            <?php echo wp_get_attachment_image( $lg, [ 200, 0 ], false, [ 
+                                'alt'     => 'Logo 404',
+                                'loading' => 'eager'
+                            ] ) ?>
+                        </div>
                     <?php endif; ?>
+
                     <?php if ($title_404 = get_field('title_404', 'option')): ?>
                         <h1 class="not-found__title"><?php echo $title_404; ?></h1>
                     <?php endif; ?>
+
                     <?php if ($text_404 = get_field('text_404', 'option')): ?>
                         <div class="not-found__subtitle"><?php echo $text_404; ?></div>
                     <?php endif; ?>
+                    
                     <div class="not-found__btn-wrapper">
                         <a class="not-found__btn btn-primary"
                            href="<?php echo home_url('/') ?>"><?php echo __('Go Home', 'datizy'); ?></a>
